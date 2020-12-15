370a6913eb2791d819d62117b3ad37c31416314e
yuping <yuping@zhonglanmedia.com>
2020-12-15 20:18:01 +0800

    init

--- src/utils/Const.js
+++ src/utils/Const.js
@@ -1,6 +1,4 @@
-export const APP_ID='wx91fad2501e704f40'
+export const APP_ID='wxc3affa5af72a961e'
 export const POST='POST'
 export const GET='GET'
-export const TEMPLATE_IDS = ['61re8ZPs9iS_fFx8W1Dm62pGfMQ_Pp0pdbovgs3cI-E']
 export const BOTTOM_GAP = 10
-export const WX_KEY = '2GOBZ-3ECRI-I6CGO-5YPX5-SDXDK-ZWBSL'
